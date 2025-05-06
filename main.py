46e53c75ec31d7e1c6ae005efe826cf1e83e82a4
nilesh3675 <nileshpatil3675@gmail.com>
2025-05-06 18:09:06 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 from src.cnnClassifier import logger
-logger.info("Wellcome to info")
+from src.cnnClassifier.utils.comman import get_size
+from src.cnnClassifier.entity.config import DataIngestionConfig
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.data_ingestion_01 import DataIngestion
+from src.cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.entity.config import PrepareBaseModelConfig
+from src.cnnClassifier.components.prepare_base_model_02 import PrepareBaseModel
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("D:\DL_project\kidneyproject_DLproject\config\config.yaml")
+PARAMS_FILE_PATH = Path("D:\DL_project\kidneyproject_DLproject\params.yaml")
